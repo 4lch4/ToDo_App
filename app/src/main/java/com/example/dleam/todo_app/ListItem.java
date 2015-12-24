ffe358b5e5372550a93458380f1f2aebf51763cf
DevinL <devinl@sapien.com>
2015-12-23 18:26:11 -0800

    Edit Items Functional
    Allows for editing of task items and updates them accordingly.

--- app/src/main/java/com/example/dleam/todo_app/ListItem.java
+++ app/src/main/java/com/example/dleam/todo_app/ListItem.java
@@ -1,14 +1,19 @@
 package com.example.dleam.todo_app;
 
+
+import java.io.Serializable;
+
 /**
  * Created by dleam on 12/22/2015.
  */
-public class ListItem {
-    public String mContent;
+public class ListItem implements Serializable {
+    private static final long serialVersionUID = 1L;
+    public String content;
+    public int position;
 
     public ListItem(String content) {
-        this.mContent = content;
+        this.content = content;
     }
 
-    public String getContent() { return mContent; }
+    public String getContent() { return content; }
 }
--- app/src/main/java/com/example/dleam/todo_app/MainActivity.java
+++ app/src/main/java/com/example/dleam/todo_app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.dleam.todo_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -11,15 +12,25 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private final int REQUEST_CODE = 42;
     private ArrayList<ListItem> mItemList;
     private CustomAdapter mCustomAdapter;
     private ListView mListView;
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+            Bundle extras = data.getExtras();
+            ListItem item = (ListItem) extras.getSerializable("item");
+            mItemList.set(item.position, item);
+            mCustomAdapter.notifyDataSetChanged();
+        }
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         mListView = (ListView) findViewById(R.id.listView);
         mItemList = new ArrayList<>();
@@ -28,10 +39,15 @@ public class MainActivity extends AppCompatActivity {
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Bundle bundle = new Bundle();
                 ListItem item = (ListItem) mCustomAdapter.getItem(position);
-                Log.i("OUTPUT", item.getContent());
+                bundle.putSerializable("item", item);
+                Intent intent = new Intent(getApplicationContext(), EditActivity.class);
+                intent.putExtras(bundle);
+                startActivityForResult(intent, REQUEST_CODE);
             }
         });
+
         mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
@@ -46,6 +62,8 @@ public class MainActivity extends AppCompatActivity {
     public void addTodo(View view) {
         TextView content = (TextView) findViewById(R.id.editText);
         ListItem item = new ListItem(content.getText().toString());
+        // Set the position of the item for editing
+        item.position = mListView.getCount();
         mItemList.add(item);
         mCustomAdapter.notifyDataSetChanged();
         content.setText(null);
