0f21549ae8a16250c2aeec028a8ec4ca223ea310
Devin Leaman <dleaman212@gmail.com>
2015-12-31 18:17:30 -0800

    Adapter Updated
    Added DueDate and Priority to the custom Adapter so it is displayed in
    the ListView.

--- app/src/main/java/com/example/dleam/todo_app/adapters/TodoTaskAdapter.java
+++ app/src/main/java/com/example/dleam/todo_app/adapters/TodoTaskAdapter.java
@@ -1,6 +1,7 @@
 package com.example.dleam.todo_app.adapters;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -39,11 +40,24 @@ public class TodoTaskAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         View view = convertView;
         if (view == null) { view = sInflater.inflate(R.layout.task_row, null); }
+        String priorityText = mTaskList.get(position).priority + " Priority";
 
-        TextView taskTitle = (TextView) view.findViewById(R.id.contentView);
+        TextView taskTitle = (TextView) view.findViewById(R.id.title_view);
+        TextView taskDueDate = (TextView) view.findViewById(R.id.date_view);
+        TextView taskPriority = (TextView) view.findViewById(R.id.priority_view);
 
         taskTitle.setText(mTaskList.get(position).title);
+        taskDueDate.setText(mTaskList.get(position).dueDate);
+        taskPriority.setText(priorityText);
 
+        switch (mTaskList.get(position).priority) {
+            case "High":
+                taskPriority.setTextColor(Color.parseColor("#E53935"));
+                break;
+            case "Low":
+                taskPriority.setTextColor(Color.parseColor("#43A047"));
+                break;
+        }
         return view;
     }
 }
